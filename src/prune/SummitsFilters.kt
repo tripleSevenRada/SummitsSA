0190ced119fecafb4edad7c2d34d8c864fc57665
Radim Vecerek <radvecerek@gmail.com>
2019-10-12 17:44:47 +0200

    elevationPoints

--- src/prune/SummitsFilters.kt
+++ src/prune/SummitsFilters.kt
@@ -4,16 +4,22 @@ import data.ElevationPoint
 import dataClasses.Elevation
 import dataClasses.Location
 
-class SummitsFilterUpDown(val up: Double, val down: Double, val locations: List<Location>) : SummitsFilter {
+const val UP_AND_DOWN_DEFAULT = 25.0
+class SummitsFilterUpDown(val upAndDown: Double = UP_AND_DOWN_DEFAULT, val locations: List<Location>) : SummitsFilter {
     override fun filterSummits(points: List<ElevationPoint>): List<ElevationPoint> {
         TODO()
     }
 }
 
+fun intervalIsValid(left: ElevationPoint, right: ElevationPoint, locations: List<Location>){
+    
+}
+
+@Throws(IllegalArgumentException::class)
 fun getLocalMin(A: ElevationPoint, B: ElevationPoint, locations: List<Location>): Double {
-    val minLoc = locations.subList(A.index, B.index).minBy { (it.elevation as Elevation.Value).elevation }
-    return if (minLoc == null) (locations[A.index].elevation as Elevation.Value).elevation else
-        (minLoc.elevation as Elevation.Value).elevation
+    val minLoc = locations.subList(A.index, (B.index +1)).minBy { (it.elevation as Elevation.Value).elevation } ?:
+        throw java.lang.IllegalArgumentException("min loc is null")
+    return (minLoc.elevation as Elevation.Value).elevation
 }
 
 fun convertHitsMapToListSummits(locations: List<Location>, hitsMap: Map<Int, Int>): List<ElevationPoint> {
--- src/test/SummitsFiltersTests.kt
+++ src/test/SummitsFiltersTests.kt
@@ -41,13 +41,19 @@ class SummitsFiltersTests {
         locations.add(Location(1.0, 2.0, Elevation.Value(7.0)))
         locations.add(Location(1.0, 2.0, Elevation.Value(10.0)))
         locations.add(Location(1.0, 2.0, Elevation.Value(11.0)))
-        locations.add(Location(1.0, 2.0, Elevation.Value(12.0))) // 6
+        locations.add(Location(1.0, 2.0, Elevation.Value(6.0))) // 6
 
         val point1 = ElevationPoint.Summit(9.0, 1)
         val point2 = ElevationPoint.Summit(11.0, 5)
 
         val minExpectValid = getLocalMin(point1, point2, locations)
         assertEquals(7.0, minExpectValid)
+
+        val point3 = ElevationPoint.Summit(10.0,0)
+        val point4 = ElevationPoint.Summit(6.0,6)
+
+        val minExpectValidFullScan = getLocalMin(point3, point4, locations)
+        assertEquals(6.0, minExpectValidFullScan)
 
         // (expected = IllegalArgumentException::class)
         val minExpectInvalid = getLocalMin(point2, point1, locations)
